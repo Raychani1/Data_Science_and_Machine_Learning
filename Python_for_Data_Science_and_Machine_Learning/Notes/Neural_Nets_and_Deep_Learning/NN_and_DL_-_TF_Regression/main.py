8a8466d774055bf8cd041efc73f02ed3cb1360e5
Raychani1 <lacko9929@gmail.com>
2021-10-14 13:05:31 +0200

    Add Tensorflow Regression Code Along 1

---
+++ Python_for_Data_Science_and_Machine_Learning/Notes/Neural_Nets_and_Deep_Learning/NN_and_DL_-_TF_Regression/main.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# from sklearn.preprocessing import MinMaxScaler
+# from sklearn.model_selection import train_test_split
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from sklearn.metrics import mean_absolute_error, mean_squared_error
+# from tensorflow.keras.models import load_model
+
+# Read the data to DataFrame
+df = pd.read_csv('data/kc_house_data.csv')
+
+# Check for NaN values
+print(f'Number of NaN values in columns:\n{df.isnull().sum()}')
+
+# Check on some information
+description = df.describe().transpose()
+print(description)
+
+# Visualization
+
+# Check the distribution of the label ( what we want to predict )
+# As you can see on the plot there are some houses that are way expensive
+# and those houses might lower our prediction accuracy, so we might think about
+# dropping them.
+plt.figure(figsize=(10, 6))
+sns.displot(df['price'])
+# plt.show()
+
+# As you can see on the count plot some houses have > 7 bedrooms
+# ( some have 33 ), which is exactly the same scenario as with the very
+# expensive houses.
+plt.figure(figsize=(10, 6))
+sns.countplot(df['bedrooms'])
+# plt.show()
+
+# We can look for correlation in the data. Since we are looking for factors that
+# are correlated with our label 'price', we can call the following command.
+# We can see that the most correlation is with price ( obviously ), but the
+# second most correlated feature is 'sqft_living'.
+print(df.corr()['price'].sort_values())
+
+# We can inspect highly correlated features through a scatter plot
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='price', y='sqft_living', data=df)
+plt.show()
+
+# This boxplot shows us the large variety of prices based on the number of
+# bedrooms.
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='bedrooms', y='price', data=df)
+plt.show()
